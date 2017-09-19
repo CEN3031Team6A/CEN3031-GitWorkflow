384c89cd85efc30f106f4754019c31c1471986db
Anthony Codella <codellaanthony@gmail.com>
2017-09-19 13:39:12 -0400

    division

--- Calculator.js
+++ Calculator.js
@@ -8,20 +8,19 @@ function subtract(){
 	var v1=parseInt(document.getElementById("input1").value);
 	var v2=parseInt(document.getElementById("input2").value);
 	alert("Result= " + (v1-v2).toString());
-	
-	
+
+
 }
 
 function multiply(){
 	//Implement this
-	
-	
+
+
 }
 
 function divide(){
-	//Implement this
-	
-	
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1/v2).toString());
+
 }
-		
